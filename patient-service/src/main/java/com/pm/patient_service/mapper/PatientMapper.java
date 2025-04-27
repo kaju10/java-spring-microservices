f6925102b06e0fce1cb00cdf72fea9f9d47b95e4
Aritra <aritramondal812second@gmail.com>
2025-04-27 08:29:59 +0530

    added dto and mapper classes

---
+++ patient-service/src/main/java/com/pm/patient_service/mapper/PatientMapper.java
@@ -0,0 +1,33 @@
+package com.pm.patient_service.mapper;
+
+import com.pm.patient_service.dto.PatientRequestDTO;
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.model.Patient;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class PatientMapper {
+
+    public static PatientResponseDTO toDTO(Patient patient){
+        PatientResponseDTO patientDTO = new PatientResponseDTO();
+        patientDTO.setName(patient.getName());
+        patientDTO.setId(patient.getId().toString());
+        patientDTO.setEmail(patient.getEmail());
+        patientDTO.setAddress(patient.getAddress());
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+
+        return patientDTO;
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO){
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+
+        return patient;
+    }
+}
--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -1,14 +1,32 @@
 package com.pm.patient_service.service;
 
+import com.pm.patient_service.dto.PatientRequestDTO;
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.mapper.PatientMapper;
+import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PatientService {
 
-    private PatientRepository patientRepository;
+    private final PatientRepository patientRepository;
 
     public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+        List<PatientResponseDTO> patientResponseDTOs = patients.stream().map(PatientMapper::toDTO).toList();
+        return patientResponseDTOs;
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        Patient patient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+        return PatientMapper.toDTO(patient);
+
     }
 }
